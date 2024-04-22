7ed393f4dd81630ecde504f5696c4cac613d38d3
Ana <flutegurl108@hotmail.com>
2024-04-22 17:14:00 -0400

    Added sqlplus script

--- rql.py
+++ rql.py
@@ -1,5 +1,8 @@
 import sys
 import subprocess
+import re
+
+list_of_sql_only_words = ['delete', 'drop', 'truncate', 'update', 'alter', 'create', 'insert']
 
 def main():
     sys.ps1 = 'RQL'
@@ -15,7 +18,7 @@ def main():
         if user_input.strip() == 'exit' or user_input.strip() == 'exit;':
             break
         if ";" in user_input:
-            processes_input(user_input)
+            processes_input(user_input.lower(), username, password)
             user_input = ""
 
 
@@ -24,8 +27,18 @@ def set_credentials(username, password):
     with open(file, 'w') as filetowrite:
         filetowrite.write(username + "\n" + password + "\n")
 
-def processes_input(user_str):
+def is_sql_terms(text_to_check):
+    for keyword in list_of_sql_only_words:
+        if keyword in text_to_check:
+            return True
+    return False
+
+def processes_input(user_str, username, password):
+    if len(user_str.strip()) == 0:
+        print("Synax Error")
+        return
     first_char = user_str.strip()[0]
+    first_word = user_str.strip().split()[0]
     if first_char == '{':
         first_word = user_str.strip()[1:].strip().split()[0]
         if '.' in first_word: # it is TRC if its not a variable
@@ -33,8 +46,24 @@ def processes_input(user_str):
         else:
             call_exec(user_str, 'drc', 'statement:')
     else:
-        call_exec(user_str, 'alg', 'ALG> \n')
-        # it is either alg or sql
+        if 'select' in first_word and 'from ' in user_str:
+            call_sqlplus(username, password, user_str)
+        elif is_sql_terms(user_str):
+            call_sqlplus(username, password, user_str)
+        else:
+            call_exec(user_str, 'alg', 'ALG> \n')
+
+def call_sqlplus(username, password, query_input):
+    file='query.txt' 
+    with open(file, 'w') as filetowrite:
+        filetowrite.write(query_input + "\nexit;")
+    ps = subprocess.Popen(['sqlplus', '-L', '-S', username + '/' + password + "@xe", "@query.txt"],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+    output = ps.communicate()[0]
+    if len(output.strip()) == 0:
+        print("Syntax error. Try again")
+        return
+
+    print output
 
 def call_exec(query_input, type_of_exe, str_to_truncate_from):
     file='query.txt' 
@@ -46,6 +75,10 @@ def call_exec(query_input, type_of_exe, str_to_truncate_from):
     start_idx = str(output).rfind(str_to_truncate_from)
     end_idx = str(output).rfind('processed.')
     result_to_print = output[(start_idx):end_idx]
+    if len(result_to_print.strip()) == 0:
+        print("Syntax error. Try again")
+        return
+
     print result_to_print
 
 if __name__ == '__main__':
